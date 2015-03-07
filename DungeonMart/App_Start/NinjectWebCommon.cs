e33052c52338ff70505980d5fd950422d0589ff7
Bill DeLude <qanwi1970@gmail.com>
2015-03-07 01:01:16 -0700

    nuget updates

--- DungeonMart/App_Start/NinjectWebCommon.cs
+++ DungeonMart/App_Start/NinjectWebCommon.cs
@@ -9,6 +9,7 @@ using DungeonMart.Services;
 using DungeonMart.Services.Interfaces;
 using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 using Ninject;
+using Ninject.Extensions.Conventions;
 using Ninject.Web.Common;
 
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
@@ -16,28 +17,28 @@ using Ninject.Web.Common;
 
 namespace DungeonMart
 {
-    public static class NinjectWebCommon 
+    public static class NinjectWebCommon
     {
-        private static readonly Bootstrapper Bootstrapper = new Bootstrapper();
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
-            Bootstrapper.Initialize(CreateKernel);
+            bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
         public static void Stop()
         {
-            Bootstrapper.ShutDown();
+            bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -89,6 +90,13 @@ namespace DungeonMart
             kernel.Bind<ISpellService>().To<SpellService>();
             kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InThreadScope();
             kernel.Bind<IDungeonMartContext>().To<DungeonMartContext>();
-        }        
+            // doesn't seem to work
+            //kernel.Bind(x =>
+            //{
+            //    x.FromThisAssembly() // Scans currently assembly
+            //        .SelectAllClasses() // Retrieve all non-abstract classes
+            //        .BindDefaultInterface(); // Binds the default interface to them;
+            //});
+        }
     }
 }
