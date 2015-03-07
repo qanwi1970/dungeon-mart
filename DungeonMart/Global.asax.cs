21067beef2a9d9a8927aa116a761c9caede598f6
Bill DeLude <qanwi1970@gmail.com>
2015-03-07 01:22:32 -0700

    Wow, so much work for basic mvc

--- DungeonMart/Global.asax.cs
+++ DungeonMart/Global.asax.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
+using System.Web.Http;
 
 namespace DungeonMart
 {
@@ -12,6 +10,11 @@ namespace DungeonMart
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
