e893da8d400ddd5d141cace380022df1ec3b3a60
Bill DeLude <qanwi1970@gmail.com>
2015-04-02 09:28:05 -0600

    Add ability stat helper

---
+++ DungeonMart.UnitTests/Services/Helpers/AbilityHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using DungeonMart.Services.Helpers;
+using NUnit.Framework;
+
+namespace DungeonMart.UnitTests.Services.Helpers
+{
+    [TestFixture]
+    public class AbilityHelperTests
+    {
+        [Test]
+        public void GetModifierLowStatTest()
+        {
+            var modifier = AbilityHelper.GetModifier(6);
+
+            Assert.AreEqual(-2, modifier);
+        }
+
+        [Test]
+        public void GetModifierAvgTest()
+        {
+            var modifier = AbilityHelper.GetModifier(11);
+
+            Assert.AreEqual(0, modifier);
+        }
+
+        [Test]
+        public void GetModifierHighStatTest()
+        {
+            var modifier = AbilityHelper.GetModifier(20);
+
+            Assert.AreEqual(5, modifier);
+        }
+    }
+}
---
+++ DungeonMart/Services/Helpers/AbilityHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace DungeonMart.Services.Helpers
+{
+    public class AbilityHelper
+    {
+        public static int GetModifier(int ability)
+        {
+            return (ability - 10)/2;
+        }
+    }
+}
