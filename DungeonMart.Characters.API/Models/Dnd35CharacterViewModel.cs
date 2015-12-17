e2b2fd6687d912658cd2caa16c9ffa42be32c614
qanwi1970 <qanwi1970@gmail.com>
2015-12-16 22:37:12 -0700

    expand 3.5 data model

---
+++ DungeonMart.Characters.API/Models/Dnd35CharacterViewModel.cs
@@ -0,0 +1,155 @@
+﻿namespace DungeonMart.Characters.API.Models
+{
+    public class Dnd35CharacterViewModel : BaseCharacterViewModel
+    {
+        public ClassLevel[] Classes { get; set; }
+        public int Ecl { get; set; }
+        public string Race { get; set; }
+        public string Alignment { get; set; }
+        public string Diety { get; set; }
+        public string Size { get; set; }
+        public int? Age { get; set; }
+        public string Gender { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public string Eyes { get; set; }
+        public string Hair { get; set; }
+        public string Skin { get; set; }
+        public Ability Strength { get; set; }
+        public Ability Dexterity { get; set; }
+        public Ability Constitution { get; set; }
+        public Ability Intelligence { get; set; }
+        public Ability Wisdom { get; set; }
+        public Ability Charisma { get; set; }
+        public int? HitPoints { get; set; }
+        public int? Speed { get; set; }
+        public ArmorClassModifiers ArmorClass { get; set; }
+        public string DamageReduction { get; set; }
+        public InitiativeModifier Initiative { get; set; }
+        public GrappleModifier Grapple { get; set; }
+        public SavingThrow FortitudeSave { get; set; }
+        public SavingThrow ReflexSave { get; set; }
+        public SavingThrow WillSave { get; set; }
+        public string[] ConditionalSaveModifiers { get; set; }
+        public int? BaseAttackBonus { get; set; }
+        public AttackStats[] AttackOptions { get; set; }
+        public SkillStats[] Skills { get; set; }
+        public int? ExperiencePoints { get; set; }
+        public ArmorStats Armor { get; set; }
+        public ShieldStats Shield { get; set; }
+        public ItemStats[] Items { get; set; }
+        public string[] RacialTraits { get; set; }
+        public string[] ClassFeatures { get; set; }
+        public string[] Feats { get; set; }
+        public string[] Languages { get; set; }
+
+
+        public class ClassLevel
+        {
+            public string ClassName { get; set; }
+            public int? Level { get; set; }
+        }
+
+        public class Ability
+        {
+            public int? Score { get; set; }
+            public int? Modifier { get; set; }
+        }
+
+        public class ArmorClassModifiers
+        {
+            public int? ArmorBonus { get; set; }
+            public int? ShieldBonus { get; set; }
+            public int? DexModifier { get; set; }
+            public int? SizeModifier { get; set; }
+            public int? NaturalArmor { get; set; }
+            public int? DeflectionModifier { get; set; }
+            public int? MiscModifier { get; set; }
+            public int? Total { get; set; }
+            public int? Touch { get; set; }
+            public int? FlatFooted { get; set; }
+        }
+
+        public class InitiativeModifier
+        {
+            public int? DexModifier { get; set; }
+            public int? MiscModifier { get; set; }
+            public int? Total { get; set; }
+        }
+
+        public class GrappleModifier
+        {
+            public int? BaseAttackBonus { get; set; }
+            public int? StrengthModifier { get; set; }
+            public int? SizeModifier { get; set; }
+            public int? MiscModifier { get; set; }
+            public int? Total { get; set; }
+        }
+
+        public class SavingThrow
+        {
+            public int? Base { get; set; }
+            public int? AbilityModifier { get; set; }
+            public int? MagicModifier { get; set; }
+            public int? MiscModifier { get; set; }
+            public int? TempModifier { get; set; }
+        }
+
+        public class AttackStats
+        {
+            public string Name { get; set; }
+            public int? AttackBonus { get; set; }
+            public string Damage { get; set; }
+            public string Critical { get; set; }
+            public string DamageType { get; set; }
+            public int? RangeIncrement { get; set; }
+            public string Notes { get; set; }
+            public int? Ammunition { get; set; }
+        }
+
+        public class SkillStats
+        {
+            public string Name { get; set; }
+            public bool ClassSkill { get; set; }
+            public string KeyAbility { get; set; }
+            public bool ArmorCheck { get; set; }
+            public int? Ranks { get; set; }
+            public int? AbilityModifier { get; set; }
+            public int? MiscModifier { get; set; }
+            public int? TotalModifier { get; set; }
+        }
+
+        public class ArmorStats
+        {
+            public string Name { get; set; }
+            public string ArmorType { get; set; }
+            public int? AcBonus { get; set; }
+            public int? MaxDex { get; set; }
+            public int? CheckPenalty { get; set; }
+            public int? SpellFailure { get; set; }
+            public int? Speed { get; set; }
+            public int? Weight { get; set; }
+            public string SpecialProperties { get; set; }
+        }
+
+        public class ShieldStats
+        {
+            public string Name { get; set; }
+            public int? AcBonus { get; set; }
+            public int? MaxDex { get; set; }
+            public int? CheckPenalty { get; set; }
+            public int? SpellFailure { get; set; }
+            public int? Weight { get; set; }
+            public string SpecialProperties { get; set; }
+        }
+
+        public class ItemStats
+        {
+            public string Name { get; set; }
+            public string Location { get; set; }
+            public decimal Weight { get; set; }
+            public string SpecialProperties { get; set; }
+        }
+    }
+
+}
